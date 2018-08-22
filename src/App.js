a6ca53d3c276a25060bd2db4072a35f74320b3c5
Joe Kowal <3199871+JoeKowal@users.noreply.github.com>
2018-08-22 11:50:00 -0400

    Add render elements to app.js

--- src/App.js
+++ src/App.js
@@ -6,60 +6,111 @@ import Search from "./Search";
 import "./App.css";
 
 class BooksApp extends React.Component {
-    MAX_RESULTS = 30;
+  MAX_RESULTS = 30;
 
-    state = {
-        books: [],
-        searchBooks: []
-    };
+  state = {
+    books: [],
+    searchBooks: []
+  };
 
-    componentDidMount() {
-        this.fetchBooks();
-    }
+  componentDidMount() {
+    this.fetchBooks();
+  }
 
-    fetchBooks() {
-        BooksAPI.getAll().then((books) => {
-            this.setState({books});
-        });
-    }
+  fetchBooks() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
 
-    getShelfBooks(shelfName){
-        return this.state.books.filter((b) => b.shelf === shelfName)
-    }
+  getShelfBooks(shelfName) {
+    return this.state.books.filter(b => b.shelf === shelfName);
+  }
 
-    changeShelf = (book, newShelf) => {
-        BooksAPI.update(book, newShelf).then(() => {
-            //local copy of book
-            book.shelf = newShelf;
+  changeShelf = (book, newShelf) => {
+    BooksAPI.update(book, newShelf).then(() => {
+      //local copy of book
+      book.shelf = newShelf;
 
-            //filter book and append
-            this.setState(state => ({
-                books: state.books.filter(b => b.id !== book.id).concat([ book ])
-            }));
-        });
-    };
+      //filter book and append
+      this.setState(state => ({
+        books: state.books.filter(b => b.id !== book.id).concat([book])
+      }));
+    });
+  };
 
-    updateQuery = (query) => {
-        if(query){
-            BooksAPI.search(query, this.MAX_RESULTS).then((books) => {
-                if(books.length){
-                    books.forEach((book, index) => {
-                        let myBook = this.state.books.find((b) => b.id === book.id);
-                        book.shelf = myBook ? myBook.shelf : 'none';
-                        books[index] = book;
-                    });
+  updateQuery = query => {
+    if (query) {
+      BooksAPI.search(query, this.MAX_RESULTS).then(books => {
+        if (books.length) {
+          books.forEach((book, index) => {
+            let myBook = this.state.books.find(b => b.id === book.id);
+            book.shelf = myBook ? myBook.shelf : "none";
+            books[index] = book;
+          });
 
-                    this.setState({
-                        searchBooks: books
-                    });
-                }
-
-            });
-            } else {
-            this.setState({
-                searchBooks: []
-            });
+          this.setState({
+            searchBooks: books
+          });
         }
-    };
+      });
+    } else {
+      this.setState({
+        searchBooks: []
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>My Books</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Shelf
+                    title="Currently Reading"
+                    books={this.getShelfBooks("currentlyReading")}
+                    changeShelf={this.changeShelf}
+                  />
+                  <Shelf
+                    title="Want to Read"
+                    books={this.getShelfBooks("wantToRead")}
+                    changeShelf={this.changeShelf}
+                  />
+                  <Shelf
+                    title="Have Read"
+                    books={this.getShelfBooks("read")}
+                    changeShelf={this.changeShelf}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search">Add a book</Link>
+              </div>
+            </div>
+          )}
+        />
+
+        <Route
+          path="/search"
+          render={({ history }) => (
+            <Search
+              books={this.state.searchBooks}
+              updateQuery={this.updateQuery}
+              changeShelf={this.changeShelf}
+            />
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 export default BooksApp;
