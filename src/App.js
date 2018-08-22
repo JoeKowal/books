9236f77f6642eda120330ca4de59e58c6bb05524
Joe Kowal <3199871+JoeKowal@users.noreply.github.com>
2018-08-22 11:21:55 -0400

    Add fetch books and change shelf to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ import Shelf from "./Shelf";
 import Search from "./Search";
 import "./App.css";
 
-class BooksApp extends React.Component {}
+class BooksApp extends React.Component {
+    MAX_RESULTS = 30;
+
+    state = {
+        books: [],
+        searchBooks: []
+    };
+
+    componentDidMount() {
+        this.fetchBooks();
+    }
+
+    fetchBooks() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books});
+        });
+    }
+
+    getShelfBooks(shelfName){
+        return this.state.books.filter((b) => b.shelf === shelfName)
+    }
+
+    changeShelf = (book, newShelf) => {
+        BooksAPI.update(book, newShelf).then(() => {
+            //local copy of book
+            book.shelf = newShelf;
+
+            //filter book and append
+            this.setState(state => ({
+                books: state.books.filter(b => b.id !== book.id).concat([ book ])
+            }));
+        });
+    };
 
 export default BooksApp;
